8cd29d0a1b3f4aab902a6c733e969f2fe574dc8c
Guillem96 <guillem.orellana@gmail.com>
2017-07-27 17:17:13 +0200

    Add project files.

---
+++ InstagramGot/Auth/InstagramCredentials.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramGot.Auth
+{
+    class InstagramCredentials
+    {
+        private string clientID;
+        private string clientSecret;
+
+        /// <param name="clientID">Client ID from api application</param>
+        /// <param name="clientSecret">Client Secret Id from api application</param>
+        public InstagramCredentials(string clientID, string clientSecret)
+        {
+            this.clientID = clientID;
+            this.clientSecret = clientSecret;
+        }
+    }
+}
